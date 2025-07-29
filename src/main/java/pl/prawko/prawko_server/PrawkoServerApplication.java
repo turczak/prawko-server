9734c229ca035aa4d48bad8204db92c80fd7f5e6
turczak <wojciech.turczak97@gmail.com>
2025-07-30 00:23:55 +0200

    Initial commit

---
+++ src/main/java/pl/prawko/prawko_server/PrawkoServerApplication.java
@@ -0,0 +1,13 @@
+package pl.prawko.prawko_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrawkoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrawkoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/prawko/prawko_server/PrawkoServerApplicationTests.java
@@ -0,0 +1,15 @@
+package pl.prawko.prawko_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("dev")
+class PrawkoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
