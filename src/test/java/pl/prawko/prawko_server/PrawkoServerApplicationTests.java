bd7372860a6cf7fe6ca5c095134563c7a7eddf10
turczak <wojciech.turczak97@gmail.com>
2025-08-11 14:04:22 +0200

    Removed profile

--- src/test/java/pl/prawko/prawko_server/PrawkoServerApplicationTests.java
+++ src/test/java/pl/prawko/prawko_server/PrawkoServerApplicationTests.java
@@ -2,10 +2,8 @@ package pl.prawko.prawko_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("dev")
 class PrawkoServerApplicationTests {
 
     @Test
