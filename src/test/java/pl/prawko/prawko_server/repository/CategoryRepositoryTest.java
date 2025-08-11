d526596d116acd93ee16953229c2166ec6fbeb32
turczak <wojciech.turczak97@gmail.com>
2025-08-11 13:38:17 +0200

    Removed profile

--- src/test/java/pl/prawko/prawko_server/repository/CategoryRepositoryTest.java
+++ src/test/java/pl/prawko/prawko_server/repository/CategoryRepositoryTest.java
@@ -4,14 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
 import pl.prawko.prawko_server.model.Category;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@ActiveProfiles("test")
 class CategoryRepositoryTest {
 
     @Autowired
