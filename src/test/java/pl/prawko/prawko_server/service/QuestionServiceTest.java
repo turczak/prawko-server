f5a91d88f0ba0f5f808ed1fc453551c92c3b79d7
turczak <w.turczak254@gmail.com>
2025-08-18 16:47:46 +0200

    Added QuestionService and merged functionality of CSVMappingUtil into it.

--- src/test/java/pl/prawko/prawko_server/service/QuestionServiceTest.java
+++ src/test/java/pl/prawko/prawko_server/service/QuestionServiceTest.java
@@ -1,4 +1,4 @@
-package pl.prawko.prawko_server.util;
+package pl.prawko.prawko_server.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,8 +10,11 @@ import org.springframework.mock.web.MockMultipartFile;
 import pl.prawko.prawko_server.mapper.AnswerMapper;
 import pl.prawko.prawko_server.mapper.QuestionMapper;
 import pl.prawko.prawko_server.model.Language;
+import pl.prawko.prawko_server.model.Question;
+import pl.prawko.prawko_server.repository.QuestionRepository;
 import pl.prawko.prawko_server.service.implementation.CategoryService;
 import pl.prawko.prawko_server.service.implementation.LanguageService;
+import pl.prawko.prawko_server.service.implementation.QuestionService;
 import pl.prawko.prawko_server.test_utils.CategoryTestData;
 import pl.prawko.prawko_server.test_utils.LanguageTestData;
 
@@ -19,14 +22,16 @@ import java.io.IOException;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static pl.prawko.prawko_server.test_utils.QuestionCSVTestData.BASIC_QUESTION_CSV;
-import static pl.prawko.prawko_server.test_utils.QuestionCSVTestData.SPECIAL_QUESTION_CSV;
 import static pl.prawko.prawko_server.test_utils.QuestionTestData.BASIC_QUESTION;
 import static pl.prawko.prawko_server.test_utils.QuestionTestData.SPECIAL_QUESTION;
 
 @ExtendWith(MockitoExtension.class)
-public class CSVMappingUtilTest {
+public class QuestionServiceTest {
+
+    @Mock
+    private QuestionRepository repository;
 
     @Mock
     private CategoryService categoryService;
@@ -35,42 +40,42 @@ public class CSVMappingUtilTest {
     private LanguageService languageService;
 
     private final List<Language> languages = LanguageTestData.ALL;
-    private CSVMappingUtil csvMappingUtil;
+    private QuestionService questionService;
 
     @BeforeEach
     void setUp() {
         final var answerMapper = new AnswerMapper(languageService);
         final var questionMapper = new QuestionMapper(categoryService, languageService, answerMapper);
-        csvMappingUtil = new CSVMappingUtil(questionMapper);
+        questionService = new QuestionService(repository, questionMapper);
     }
 
     @Test
     void mapCSVFile_correctly() throws IOException {
         final ClassPathResource resource = new ClassPathResource("test_question.csv");
+        final var inputStream = resource.getInputStream();
+        final var file = new MockMultipartFile("file", "test_question.csv", "text/csv", inputStream);
+        when(categoryService.findAllFromString("A,B")).thenReturn(CategoryTestData.CATEGORIES_AB);
+        when(categoryService.findAllFromString("PT")).thenReturn(List.of(CategoryTestData.CATEGORY_PT));
+        when(languageService.findAll()).thenReturn(languages);
+        final var expected = List.of(BASIC_QUESTION, SPECIAL_QUESTION);
+
+        final var result = questionService.parseFileToQuestions(file);
+
         assertThat(resource)
                 .satisfies(res -> {
                     assertThat(res.exists()).isTrue();
                     assertThat(res.isReadable()).isTrue();
                 });
-        final var inputStream = resource.getInputStream();
-        final var file = new MockMultipartFile("file", "test_question.csv", "text/csv", inputStream);
-
-        final var result = csvMappingUtil.mapFileToQuestionCSVModels(file);
-
-        assertThat(result).isEqualTo(List.of(SPECIAL_QUESTION_CSV, BASIC_QUESTION_CSV));
+        assertThat(result).containsExactlyInAnyOrder(expected.toArray(new Question[0]));
     }
 
     @Test
-    void mapQuestionCSVModelsToQuestions_mapBothTypeOfQuestions() {
-        when(categoryService.findAllFromString("A,B")).thenReturn(CategoryTestData.CATEGORIES_AB);
-        when(categoryService.findAllFromString("PT")).thenReturn(List.of(CategoryTestData.CATEGORY_PT));
-        when(languageService.findAll()).thenReturn(languages);
-        final var given = List.of(BASIC_QUESTION_CSV, SPECIAL_QUESTION_CSV);
-        final var expected = List.of(BASIC_QUESTION, SPECIAL_QUESTION);
+    void saveAll_callsRepositorySaveAll() {
+        final var questions = List.of(BASIC_QUESTION, SPECIAL_QUESTION);
 
-        final var result = csvMappingUtil.mapQuestionCSVModelsToQuestions(given);
+        questionService.saveAll(questions);
 
-        assertThat(result).isEqualTo(expected);
+        verify(repository).saveAll(questions);
     }
 
 }
