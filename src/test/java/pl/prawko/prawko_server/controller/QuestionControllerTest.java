794f5c6bb2392dfbfa23f24ce9cab0d5f1f381d7
turczak <w.turczak254@gmail.com>
2025-08-19 08:50:38 +0200

    Added MultiPartFactory for tests

--- src/test/java/pl/prawko/prawko_server/controller/QuestionControllerTest.java
+++ src/test/java/pl/prawko/prawko_server/controller/QuestionControllerTest.java
@@ -4,14 +4,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestClient;
 import pl.prawko.prawko_server.dto.ApiResponse;
+import pl.prawko.prawko_server.test_utils.MultiPartFactory;
 import pl.prawko.prawko_server.test_utils.TestUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,6 +23,7 @@ public class QuestionControllerTest {
     private int port;
 
     private RestClient restClient;
+    private MultiValueMap<String, Object> multipart;
 
     @BeforeEach
     void setUp() {
@@ -36,11 +35,12 @@ public class QuestionControllerTest {
     @Test
     void addQuestions_throwBadRequest_whenFileIsMissing() {
         final var expected = "Required part 'file' is not present.";
+        multipart = MultiPartFactory.empty();
 
         final var response = restClient.post()
                 .uri(URL)
                 .contentType(MediaType.MULTIPART_FORM_DATA)
-                .body(new LinkedMultiValueMap<>())
+                .body(multipart)
                 .exchange((req, res) -> TestUtils.getResponseEntity(res));
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
@@ -50,20 +50,12 @@ public class QuestionControllerTest {
     @Test
     void addQuestions_throwUnsupportedMediaType_whenFileFormatIsWrong() {
         final var expected = "Invalid file format.";
-        final var fileResource = new ByteArrayResource(new byte[0]) {
-            @Override
-            public String getFilename() {
-                return "wrong-file.txt";
-            }
-        };
-
-        final var parts = new LinkedMultiValueMap<String, Object>();
-        parts.add("file", fileResource);
+        multipart = MultiPartFactory.withWrongFile();
 
         final var response = restClient.post()
                 .uri(URL)
                 .contentType(MediaType.MULTIPART_FORM_DATA)
-                .body(parts)
+                .body(multipart)
                 .exchange((req, res) -> TestUtils.getResponseEntity(res));
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNSUPPORTED_MEDIA_TYPE);
@@ -73,14 +65,12 @@ public class QuestionControllerTest {
     @Test
     void addQuestions_returnOk_whenSuccess() {
         final var expected = "Questions from file added successfully.";
-        final ClassPathResource resource = new ClassPathResource("test_question.csv");
-        final MultiValueMap<String, Object> parts = new LinkedMultiValueMap<>();
-        parts.add("file", resource);
+        multipart = MultiPartFactory.fromClasspath("test_question.csv");
 
         final var response = restClient.post()
                 .uri(URL)
                 .contentType(MediaType.MULTIPART_FORM_DATA)
-                .body(parts)
+                .body(MultiPartFactory.fromClasspath("test_question.csv"))
                 .retrieve()
                 .toEntity(ApiResponse.class);
 
