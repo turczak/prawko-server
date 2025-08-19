9f669a998712998959c397f13c26d795f38acf3c
turczak <w.turczak254@gmail.com>
2025-08-19 12:26:21 +0200

    Documentation of repositories

--- src/main/java/pl/prawko/prawko_server/repository/QuestionRepository.java
+++ src/main/java/pl/prawko/prawko_server/repository/QuestionRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.prawko.prawko_server.model.Question;
 
+/**
+ * Repository for {@link Question} entities.
+ * <p>
+ * Provides standard CRUD operations through {@link JpaRepository} and custom methods.
+ * </p>
+ */
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 }
