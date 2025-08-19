5ff538e920aee8eafa077a8285b2a2322fd15dc8
turczak <w.turczak254@gmail.com>
2025-08-19 12:24:20 +0200

    Documentation of repositories

--- src/main/java/pl/prawko/prawko_server/repository/CategoryRepository.java
+++ src/main/java/pl/prawko/prawko_server/repository/CategoryRepository.java
@@ -6,9 +6,21 @@ import pl.prawko.prawko_server.model.Category;
 
 import java.util.Optional;
 
+/**
+ * Repository for {@link Category} entities.
+ * <p>
+ * Provides standard CRUD operations through {@link JpaRepository} and custom methods.
+ * </p>
+ */
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
+    /**
+     * Retrieves a category by its name.
+     *
+     * @param name the name of the category to find
+     * @return an {@link Optional} of {@link Category} if found, otherwise empty
+     */
     Optional<Category> findByName(String name);
 
 }
