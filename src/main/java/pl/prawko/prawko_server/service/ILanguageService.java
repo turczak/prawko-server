404b938bed10aaa36e78ac0419001be5a2625fcc
turczak <w.turczak254@gmail.com>
2025-08-19 12:06:23 +0200

    Documentation of LanguageService and ILanguageService

--- src/main/java/pl/prawko/prawko_server/service/ILanguageService.java
+++ src/main/java/pl/prawko/prawko_server/service/ILanguageService.java
@@ -4,8 +4,16 @@ import pl.prawko.prawko_server.model.Language;
 
 import java.util.List;
 
+/**
+ * Service interface for retrieving {@link Language} entities.
+ */
 public interface ILanguageService {
 
+    /**
+     * Returning list of all existing {@link Language} entities.
+     *
+     * @return list of {@link Language}
+     */
     List<Language> findAll();
 
 }
--- src/main/java/pl/prawko/prawko_server/service/implementation/LanguageService.java
+++ src/main/java/pl/prawko/prawko_server/service/implementation/LanguageService.java
@@ -2,20 +2,33 @@ package pl.prawko.prawko_server.service.implementation;
 
 import org.springframework.stereotype.Service;
 import pl.prawko.prawko_server.model.Language;
+import pl.prawko.prawko_server.repository.CategoryRepository;
 import pl.prawko.prawko_server.repository.LanguageRepository;
 import pl.prawko.prawko_server.service.ILanguageService;
 
 import java.util.List;
 
+/**
+ * Implementation of {@link ILanguageService} that retrieves {@link Language} entities using a {@link LanguageRepository}.
+ */
 @Service
 public class LanguageService implements ILanguageService {
 
     private final LanguageRepository repository;
 
+    /**
+     * Constructs a {@code LanguageService} with the given {@link CategoryRepository}
+     *
+     * @param repository the language repository used for data access
+     */
     public LanguageService(final LanguageRepository repository) {
         this.repository = repository;
     }
 
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public List<Language> findAll() {
         return repository.findAll();
