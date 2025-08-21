0046fffc072660863fa55942b3b01cd8f6abd65e
turczak <w.turczak254@gmail.com>
2025-08-21 14:51:29 +0200

    Documentation of QuestionController and ExceptionController

--- src/main/java/pl/prawko/prawko_server/controller/ExceptionController.java
+++ src/main/java/pl/prawko/prawko_server/controller/ExceptionController.java
@@ -8,15 +8,32 @@ import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.multipart.support.MissingServletRequestPartException;
 import pl.prawko.prawko_server.dto.ApiResponse;
 
+/**
+ * Handles exceptions in REST controllers.
+ * <p>
+ * Returns error responses using {@link ApiResponse}
+ */
 @RestControllerAdvice
 public class ExceptionController {
 
+    /**
+     * Handle cases when request is missing required file.
+     *
+     * @param exception exception thrown when a required file is missing
+     * @return an error response
+     */
     @ExceptionHandler(MissingServletRequestPartException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiResponse handleMissingFile(final MissingServletRequestPartException exception) {
         return new ApiResponse("Required part 'file' is not present.");
     }
 
+    /**
+     * Handle cases when request contain wrong type of file.
+     *
+     * @param exception exception thrown when a required file is wrong type
+     * @return an error response
+     */
     @ExceptionHandler(MultipartException.class)
     @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
     public ApiResponse handleWrongFileType(final MultipartException exception) {
--- src/main/java/pl/prawko/prawko_server/controller/QuestionController.java
+++ src/main/java/pl/prawko/prawko_server/controller/QuestionController.java
@@ -6,10 +6,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
 import pl.prawko.prawko_server.dto.ApiResponse;
 import pl.prawko.prawko_server.service.implementation.QuestionService;
 
+/**
+ * REST controller to managing questions using {@code http requests} mapped on {@code /questions}.
+ */
 @RestController
 @RequestMapping("/questions")
 public class QuestionController {
@@ -20,6 +25,17 @@ public class QuestionController {
         this.questionService = questionService;
     }
 
+    /**
+     * {@code POST} for upload questions from CSV file.
+     * <p>
+     * It's parsing provided file to questions which will be saved.
+     * <p>
+     * Errors are handled using {@link ExceptionController#handleMissingFile(MissingServletRequestPartException)}
+     * and {@link ExceptionController#handleWrongFileType(MultipartException)}.
+     *
+     * @param file provided file in request
+     * @return success if questions from file were added
+     */
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ApiResponse> addQuestions(@RequestPart MultipartFile file) {
         final var questions = questionService.parseFileToQuestions(file);
