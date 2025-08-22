e760b6e80a06c56b8c7de421d976c12f78d64421
turczak <w.turczak254@gmail.com>
2025-08-22 19:54:46 +0200

    RoleService tests

---
+++ src/test/java/pl/prawko/prawko_server/service/RoleServiceTest.java
@@ -0,0 +1,52 @@
+package pl.prawko.prawko_server.service;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.assertj.core.api.ThrowableAssert;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.prawko.prawko_server.model.Role;
+import pl.prawko.prawko_server.repository.RoleRepository;
+import pl.prawko.prawko_server.service.implementation.RoleService;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+class RoleServiceTest {
+
+    @Mock
+    private RoleRepository repository;
+
+    @InjectMocks
+    private RoleService service;
+
+    @Test
+    void findByName_returnRole_whenFound() {
+        final var role = new Role().setName("TEST");
+        when(repository.findByName(role.getName())).thenReturn(Optional.of(role));
+
+        final var result = service.findByName(role.getName());
+
+        assertThat(result).isEqualTo(role);
+    }
+
+    @Test
+    void findByName_throwNotFound_whenNotFound() {
+        final var roleName = "nonExistingRole";
+        when(repository.findByName(roleName)).thenReturn(Optional.empty());
+
+        ThrowableAssert.ThrowingCallable result = () -> service.findByName(roleName);
+
+        assertThatThrownBy(result)
+                .isInstanceOf(EntityNotFoundException.class)
+                .hasMessageContaining("Role with name 'nonExistingRole' not found.");
+    }
+
+}
