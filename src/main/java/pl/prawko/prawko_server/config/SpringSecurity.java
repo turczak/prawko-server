557a63a58e16eb485c5cd5d1dedf9b58c014c47e
turczak <w.turczak254@gmail.com>
2025-08-31 10:39:38 +0200

    Add missing annotation

--- src/main/java/pl/prawko/prawko_server/config/SpringSecurity.java
+++ src/main/java/pl/prawko/prawko_server/config/SpringSecurity.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -20,6 +21,7 @@ import org.springframework.security.web.SecurityFilterChain;
  * </ul>
  */
 @Configuration
+@EnableWebSecurity
 public class SpringSecurity {
 
     /**
