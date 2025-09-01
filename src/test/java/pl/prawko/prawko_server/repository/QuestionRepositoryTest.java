3f4a3085838a2c9d3d73ad9a46ad1faa109abf45
turczak <w.turczak254@gmail.com>
2025-09-01 13:13:01 +0200

    find all questions by type and category

--- src/test/java/pl/prawko/prawko_server/repository/QuestionRepositoryTest.java
+++ src/test/java/pl/prawko/prawko_server/repository/QuestionRepositoryTest.java
@@ -3,6 +3,7 @@ package pl.prawko.prawko_server.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import pl.prawko.prawko_server.test_utils.CategoryTestData;
 import pl.prawko.prawko_server.test_utils.QuestionTestData;
 
 import java.util.List;
@@ -12,12 +13,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DataJpaTest
 public class QuestionRepositoryTest {
 
-    private static final String[] IGNORED_FIELDS = {"answers.id",
+    private static final String[] IGNORED_FIELDS = {
+            "answers.id",
             "answers.translations.id",
             "answers.translations.language.answerTranslations",
             "answers.translations.language.questionTranslations",
             "answers.translations.language.exams",
             "translations.id",
+            "translations.language",
             "categories.questions",
             "categories.exams"};
 
@@ -31,6 +34,20 @@ public class QuestionRepositoryTest {
         final var expected = List.of(question1, question2);
         repository.saveAll(expected);
         final var result = repository.findAll();
+
+        assertThat(result)
+                .usingRecursiveComparison()
+                .ignoringFields(IGNORED_FIELDS)
+                .isEqualTo(expected);
+    }
+
+    @Test
+    void findByTypeAndCategoriesContaining() {
+        final var question = QuestionTestData.BASIC_QUESTION;
+        final var category = CategoryTestData.CATEGORY_B;
+        repository.save(question);
+        final var expected = List.of(question);
+        final var result = repository.findByTypeAndCategories_NameContains(question.getType(), category.getName());
 
         assertThat(result)
                 .usingRecursiveComparison()
