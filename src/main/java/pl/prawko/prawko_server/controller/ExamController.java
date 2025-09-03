b932040f44cfa103d28994cb23c4d8b69b9680aa
turczak <w.turczak254@gmail.com>
2025-09-03 13:11:20 +0200

    Exam controller

---
+++ src/main/java/pl/prawko/prawko_server/controller/ExamController.java
@@ -0,0 +1,43 @@
+package pl.prawko.prawko_server.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import pl.prawko.prawko_server.dto.ApiResponse;
+import pl.prawko.prawko_server.dto.CreateExamRequest;
+import pl.prawko.prawko_server.model.Exam;
+import pl.prawko.prawko_server.service.implementation.ExamService;
+
+/**
+ * REST controller to manage exams using HTTP requests.
+ * <p>
+ * Mapped on {@code /exams}
+ */
+@RestController
+@RequestMapping("/exams")
+public class ExamController {
+
+    private final ExamService service;
+
+    public ExamController(ExamService service) {
+        this.service = service;
+    }
+
+    /**
+     * POST method to create a new {@link Exam} using {@link CreateExamRequest}.
+     *
+     * @param dto the valid DTO
+     * @return status 201 with success message
+     */
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ApiResponse createExam(@RequestBody @Valid final CreateExamRequest dto) {
+        service.createExam(dto.userId(), dto.categoryName());
+        return new ApiResponse("Exam successfully created.");
+    }
+
+}
---
+++ src/main/java/pl/prawko/prawko_server/dto/CreateExamRequest.java
@@ -0,0 +1,21 @@
+package pl.prawko.prawko_server.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+/**
+ * Data Transfer Object representing create exam request with {@code userId} and {@code categoryName}
+ * <p>
+ * This DTO is used to transfer exam creation data between the API layer and the service layer.
+ */
+public record CreateExamRequest(
+
+        @NotEmpty(message = "userId is required")
+        long userId,
+
+        @NotBlank(message = "category is required")
+        @Size(min = 1, max = 2)
+        String categoryName) {
+    
+}
